a641de3f762056097f69cfce7f1b008aac8988be
shivamb1208 <151376961+shivamb1208@users.noreply.github.com>
2023-11-21 14:26:17 +0530

    Update config.py
    Signed-off-by: shivamb1208 <151376961+shivamb1208@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/shivamb1208/SAROZ/",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
